d7a7682df25ca1d29110302edd0587a13ce4b4e2
den-k-203 <novolan2014@gmail.com>
2025-07-23 22:19:15 +0300

    Update requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from service.diarization_service import process_audio_segments
 
-path='audio/test_english.mp3'
+path='audio/conversation.mp3'
 
 if __name__ == '__main__':
-    process_audio_segments(path_to_audio=path, output_json_path="test_english.json")
+    process_audio_segments(path_to_audio=path, output_json_path="conversation.json")
